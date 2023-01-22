68c9de9b5cfb152efd64b2ed443f6e3c314b4d3f
Edilson Santos <edilsonsantosexsp@gmail.com>
2023-01-22 14:33:01 -0300

    .

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -43,7 +43,7 @@ class ClientController extends Controller
 
     public function search($text)
     {
-       $texto = Client::select(['name','email','phone','id_number'])->where('name','LIKE', $text)->first();
+       $texto = Client::select(['name','email','phone','id_number'])->where('name','=', $text)->first();
 
        return response()->json($texto);
     } 
