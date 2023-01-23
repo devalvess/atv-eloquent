c678b122f01921ed2e43edd53ea38b1c745ad6c7
Edilson Santos <edilsonsantosexsp@gmail.com>
2023-01-22 22:50:08 -0300

    .

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -51,9 +51,9 @@ class ClientController extends Controller
     public function bills($client)
     {
 
-        $cliente = Client::select(['id', 'name' ,'email','id_number'])->where('name', 'LIKE', $client)->get();
+        $client = Client::select(['id', 'name' ,'email','id_number'])->where('name', '=', $client)->get();
 
-        return response()->json($cliente);
+        return response()->json($client);
 
 
     }
